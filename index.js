f6b398f43a86f32e66d7bb4c183b39da1a104569
dnyncm <dny.ncm@gmail.com>
2025-02-03 18:58:26 +0700

    Add gmail authorization

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import session from "express-session";
+import GoogleStrategy from "passport-google-oauth20";
 import env from "dotenv";
 
 const app = express();
@@ -65,6 +66,16 @@ app.get("/secrets", (req, res) => {
   }
 });
 
+app.get("/auth/google", passport.authenticate("google", {
+  scope: ["profile", "email"]
+}));
+
+app.get("/auth/google/secrets", passport.authenticate("google", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  })
+);
+
 app.post(
   "/login",
   passport.authenticate("local", {
@@ -106,7 +117,7 @@ app.post("/register", async (req, res) => {
   }
 });
 
-passport.use(
+passport.use("local",
   new Strategy(async function verify(username, password, cb) {
     try {
       const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
@@ -138,6 +149,21 @@ passport.use(
     }
   })
 );
+
+passport.use("google", new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+}, async (accessToken, refreshToken, profile, cb) => {
+  // Your verification logic here
+  console.log(profile);
+  try {
+
+  } catch (err) {
+    console.log(err);
+  }
+}));
 
 passport.serializeUser((user, cb) => {
   cb(null, user);
